7873a2bde09515b8398fe22e4094fac3bba491f9
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2018-08-07 15:26:14 +0200

    test kommentar

--- app/src/main/java/com/schach/MainMenu.java
+++ app/src/main/java/com/schach/MainMenu.java
@@ -9,5 +9,6 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        //test
     }
 }
