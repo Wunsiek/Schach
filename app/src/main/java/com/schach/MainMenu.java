c820da7b74f0724df73ed53ebf46fc860abf6755
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2018-08-07 15:30:15 +0200

    c = 0

--- app/src/main/java/com/schach/MainMenu.java
+++ app/src/main/java/com/schach/MainMenu.java
@@ -9,6 +9,7 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        int c = 0;
         //test
     }
 }
