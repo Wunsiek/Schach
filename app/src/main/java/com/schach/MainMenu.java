5947999fbb35ccd1c773bbc0cdf23091bdae9bd0
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2018-08-07 15:31:14 +0200

    alles auf zurück

--- app/src/main/java/com/schach/MainMenu.java
+++ app/src/main/java/com/schach/MainMenu.java
@@ -9,7 +9,5 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        int c = 0;
-        //test
     }
 }
