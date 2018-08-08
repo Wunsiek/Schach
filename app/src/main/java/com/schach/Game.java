77043988ef488977e430fb5247a5c9891e50c290
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2018-08-08 19:13:27 +0200

    auf anfang

--- app/src/main/java/com/schach/Game.java
+++ app/src/main/java/com/schach/Game.java
@@ -2,9 +2,6 @@ package com.schach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageButton;
-
-import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
@@ -12,12 +9,6 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        ArrayList<ImageButton> buttonList = new ArrayList<ImageButton>();
-        ImageButton imgBA8 = (ImageButton) findViewById(R.id.imageButtonA8);
-        buttonList.add(imgBA8);
-        for(ImageButton imgB : buttonList){
-            imgB.setMaxHeight(imgB.getWidth());
-        }
 
     }
 }
