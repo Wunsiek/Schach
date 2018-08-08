8539291a30216438d764a41ac0d7327dc326504e
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2018-08-08 13:42:00 +0200

    buttons abhängig von screensize testen

--- app/src/main/java/com/schach/Game.java
+++ app/src/main/java/com/schach/Game.java
@@ -2,6 +2,9 @@ package com.schach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class Game extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Game extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        ArrayList<ImageButton> buttonList = new ArrayList<ImageButton>();
+        ImageButton imgBA8 = (ImageButton) findViewById(R.id.imageButtonA8);
+        buttonList.add(imgBA8);
+        for(ImageButton imgB : buttonList){
+            imgB.setMaxHeight(imgB.getWidth());
+        }
+
     }
 }
