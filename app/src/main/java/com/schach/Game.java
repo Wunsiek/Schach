d11ce12ecbee7b48c7de0bd09b5f1a7ad4d63fd2
DanielWunder <41953388+DanielWunder@users.noreply.github.com>
2018-08-09 12:32:02 +0200

    gemalayout

--- app/src/main/java/com/schach/Game.java
+++ app/src/main/java/com/schach/Game.java
@@ -2,13 +2,15 @@ package com.schach;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Game extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-
     }
 }
